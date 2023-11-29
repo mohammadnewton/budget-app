e16163d40bbb77b99fde6152bc5cf6380a5900d1
Newton Ombese <newtonombese1@gmail.com>
2023-11-29 17:54:10 +0300

    Configure my routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
+  get 'sessions/new'
+  get 'sessions/create'
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -7,4 +12,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "splash#index"
+
+  get '/login', to: 'sessions#new', as: 'login'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy', as: 'logout'
+  
+  get '/signup', to: 'users#new', as: 'signup'
+  post '/signup', to: 'users#create'
 end
